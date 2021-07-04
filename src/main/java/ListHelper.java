c7d3a4b57eda46d553a38c7c6de24c553607e9ce
Bak <bbakam.pro@gmail.com>
2021-07-04 22:14:01 +0200

    Finalisation des méthodes exists (list et array) pour vérifier si un numéro donné existe dans la liste.

--- src/main/java/ListHelper.java
+++ src/main/java/ListHelper.java
@@ -1,18 +1,25 @@
 import java.util.List;
 
 public class ListHelper {
-
     public boolean exist(int number, int[] numberList) {
+        for (int i : numberList) {
+            if (i == (number)) {
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean exist(int number, List<Integer> numberList) {
+        if (numberList.contains(number)) {
+            return true;
+        }
         return false;
     }
 
     public static void main(String[] args) {
         ListHelper listHelper = new ListHelper();
-        int[] numbers = new int[]{0,1,2};
+        int[] numbers = new int[]{0, 1, 2};
         System.out.println(listHelper.exist(1, numbers));
     }
 }
