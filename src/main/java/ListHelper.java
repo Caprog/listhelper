00cae246ed386cfea36bf1fdcb9e60e4599bbd53
Caprog <caprog.info@gmail.com>
2021-07-05 09:24:06 +0200

    Update ListHelper.java
    On peut soigner un peu plus ton code.
    Pas besoin de créer un if quand on utilise la méthode contains d'une liste (elle retourne toujours un boolean).

--- src/main/java/ListHelper.java
+++ src/main/java/ListHelper.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class ListHelper {
     public boolean exist(int number, int[] numberList) {
         for (int i : numberList) {
-            if (i == (number)) {
+            if (i == number) {
                 return true;
             }
         }
@@ -11,10 +11,7 @@ public class ListHelper {
     }
 
     public boolean exist(int number, List<Integer> numberList) {
-        if (numberList.contains(number)) {
-            return true;
-        }
-        return false;
+        return numberList.contains(number)
     }
 
     public static void main(String[] args) {
